4f7ae62f3f9c68031d2b6a59d6030f3e897f1b8e
Emanuel3k <emanuelsantos_677@hotmail.com>
2025-07-06 01:57:25 -0300

    feat: add constants for Spotify authentication

---
+++ internal/services/auth/consts.go
@@ -0,0 +1,9 @@
+package auth
+
+const (
+	scope               = "user-read-private user-read-email"
+	spotifyClientId     = "SPOTIFY_CLIENT_ID"
+	spotifyURLCallback  = "SPOTIFY_URL_CALLBACK"
+	spotifyAccessToken  = "SPOTIFY_ACCESS_TOKEN"
+	spotifyClientSecret = "SPOTIFY_CLIENT_SECRET"
+)
